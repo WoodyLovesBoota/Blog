6984fa29fb3b406dfd10a0722afa8c355a8e6a87
woody <130626781+WoodyLovesBoota@users.noreply.github.com>
2024-11-09 17:33:20 +0900

    홈화면 레이아웃 추가

--- src/components/Header/CommonHeader/CommonHeader.tsx
+++ src/components/Header/CommonHeader/CommonHeader.tsx
@@ -4,25 +4,26 @@ import cn from "classnames/bind";
 import styles from "./CommonHeader.module.scss";
 import React from "react";
 import { ROUTES } from "@/constants/route.constant";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const cx = cn.bind(styles);
 
 const GNB_LIST = [
-  { name: "HOME", path: ROUTES.INDEX },
-  { name: "DETAIL", path: ROUTES.DETAIL },
-  { name: "TRENDING", path: ROUTES.TRENDING },
+  { name: "Home", path: ROUTES.INDEX },
+  { name: "Detail", path: ROUTES.DETAIL },
+  { name: "Trending", path: ROUTES.TRENDING },
 ];
 
 const CommonHeader = () => {
   const router = useRouter();
+  const pathname = usePathname();
 
   return (
     <nav className={cx("Wrapper")}>
       {GNB_LIST.map((link) => (
         <button
           key={link.name}
-          className={cx("NavButton")}
+          className={cx("NavButton", { current: pathname === link.path })}
           onClick={() => router.push(link.path)}
         >
           {link.name}
--- src/views/Home/Home.view.tsx
+++ src/views/Home/Home.view.tsx
@@ -5,6 +5,7 @@ import styles from "./Home.view.module.scss";
 import { getRandomSample } from "@/utils/string.utils";
 import { useEffect, useState } from "react";
 import { usePopup } from "@/components/hooks/popup/usePopup";
+import Image from "next/image";
 
 const cx = cn.bind(styles);
 
@@ -29,7 +30,74 @@ const HomeView = () => {
   };
   return (
     <div className={cx("Wrapper")}>
-      <button onClick={handleButtonClick}>popup open</button>
+      <section className={cx("Section")}>
+        <div className={cx("Title")}>
+          <p>Technology</p>
+          <h2>Frontend Technology</h2>
+        </div>
+        <main className={cx("Main")}>
+          <div className={cx("MainContent")}>
+            <Image
+              src={"/static/images/sample.png"}
+              alt="frontend"
+              width={515}
+              height={427}
+              style={{
+                height: "427px",
+                width: "515px",
+                objectFit: "cover",
+                objectPosition: "center",
+                borderRadius: "12px",
+              }}
+            />
+            <div className={cx("MainContentText")}>
+              <h3 className={cx("MainContentTitle")}>와다다곰 이란</h3>
+              <p className={cx("MainContentDescription")}>와다다곰 짱짱짱 너무너무너무 귀여워요</p>
+              <div className={cx("MainContentStatistic")}>
+                <div className={cx("MainContentStatisticItem")}>
+                  <p className={cx("MainContentStatisticItemKey")}>Category</p>
+                  <p className={cx("MainContentStatisticItemValue")}>Projects</p>
+                </div>
+                <div className={cx("MainContentStatisticItem")}>
+                  <p className={cx("MainContentStatisticItemKey")}>Publication Date</p>
+                  <p className={cx("MainContentStatisticItemValue")}>October 10, 2023</p>
+                </div>
+              </div>
+              <div className={cx("MainContentButtonWrapper")}>
+                <button className={cx("MainContentButton")}>Read More</button>
+              </div>
+            </div>
+          </div>
+        </main>
+        <section className={cx("ThreeCards")}>
+          {Array.from({ length: 3 }).map((card, index) => (
+            <div className={cx("Card")} key={index}>
+              <Image
+                src={"/static/images/sample.png"}
+                alt="frontend"
+                width={515}
+                height={222}
+                style={{
+                  height: "222px",
+                  width: "100%",
+                  objectFit: "cover",
+                  objectPosition: "center",
+                  borderRadius: "12px",
+                }}
+              />
+              <div className={cx("CardText")}>
+                <h3 className={cx("CardTitle")}>와다다곰 이란</h3>
+                <p className={cx("CardDescription")}>와다다곰 짱짱짱 너무너무너무 귀여워요</p>
+                <div className={cx("CardButtonWrapper")}>
+                  <p className={cx("CardDate")}>October 10, 2023</p>
+                  <button className={cx("CardButton")}>Read More</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
+        <section className={cx("Table")}></section>
+      </section>
     </div>
   );
 };
