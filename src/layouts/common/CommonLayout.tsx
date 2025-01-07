e6037dd34aa5b8b6bdd77879fbf509a5e8c79c35
woody <woodylovesboota@gmail.com>
2025-01-07 09:26:27 +0900

    페이징 애니메이션

--- src/layouts/common/CommonLayout.tsx
+++ src/layouts/common/CommonLayout.tsx
@@ -1,34 +1,52 @@
 "use client";
-import React, { Component } from "react";
+import React, { Component, PropsWithChildren, useContext, useRef } from "react";
 import cn from "classnames/bind";
 import styles from "./CommonLayout.module.scss";
 import AnimateLayout from "../animate/AnimateLayout";
 import { usePathname } from "next/navigation";
-
+import { AnimatePresence, motion } from "framer-motion";
+import { LayoutRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
 const cx = cn.bind(styles);
 
 interface ICommonLayoutProps {
-    header?: React.ReactNode;
-    footer?: React.ReactNode;
-    type?: "common";
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
+  type?: "common";
+}
+
+function FrozenRouter(props: PropsWithChildren<{}>) {
+  const context = useContext(LayoutRouterContext);
+  const frozen = useRef(context).current;
+
+  return (
+    <LayoutRouterContext.Provider value={frozen}>
+      {props.children}
+    </LayoutRouterContext.Provider>
+  );
 }
 
 const CommonLayout = (props: React.PropsWithChildren<ICommonLayoutProps>) => {
-    const { children, header, footer, type } = props;
-    const pathname = usePathname();
+  const { children, header, footer, type } = props;
+  const pathname = usePathname();
 
-    return (
-        <div
-            className={cx("Wrapper")}
-            style={{
-                backgroundColor: pathname === "/tech" ? "red" : "#121212",
-            }}
+  return (
+    <div className={cx("Wrapper")}>
+      {header && <header className={cx("Header")}>{header}</header>}
+      <AnimatePresence mode="wait">
+        <motion.main
+          key={pathname}
+          className={cx("Main")}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
         >
-            {header && <header className={cx("Header")}>{header}</header>}
-            <main className={cx("Main")}>{children}</main>
-            {footer && <footer className={cx("Footer")}>{footer}</footer>}
-        </div>
-    );
+          <FrozenRouter>{children}</FrozenRouter>
+        </motion.main>
+      </AnimatePresence>
+      {footer && <footer className={cx("Footer")}>{footer}</footer>}
+    </div>
+  );
 };
 
 export default CommonLayout;
--- src/views/Home/Home.view.tsx
+++ src/views/Home/Home.view.tsx
@@ -3,67 +3,67 @@
 import cn from "classnames/bind";
 import styles from "./Home.view.module.scss";
 import { useRouter } from "next/navigation";
-
+import { motion } from "framer-motion";
 const cx = cn.bind(styles);
 
 const HomeView = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleMoreClick = () => {
-        router.push("/tech");
-    };
+  const handleMoreClick = () => {
+    router.push("/tech");
+  };
 
-    return (
-        <div className={cx("Wrapper")}>
-            <div className={cx("Container")}>
-                <section className={cx("TechSection")}>
-                    <header className={cx("SectionHeader")}>
-                        <div className={cx("SectionTitleWrapper")}>
-                            <p className={cx("SectionTitle")}>{"Tech :"}</p>
-                            <button
-                                className={cx("SectionButton")}
-                                onClick={handleMoreClick}
-                            >
-                                {"More"}
-                            </button>
-                        </div>
-                        <div className={cx("SectionSubtitleWrapper")}>
-                            technical insights
-                        </div>
-                    </header>
-                    <main className={cx("SectionContent")}></main>
-                </section>
-                <section className={cx("TechSection")}>
-                    <header className={cx("SectionHeader")}>
-                        <div className={cx("SectionTitleWrapper")}>
-                            <p className={cx("SectionTitle")}>{"Tech :"}</p>
-                            <button className={cx("SectionButton")}>
-                                {"More"}
-                            </button>
-                        </div>
-                        <div className={cx("SectionSubtitleWrapper")}>
-                            technical insights
-                        </div>
-                    </header>
-                    <main className={cx("SectionContent")}></main>
-                </section>
-                <section className={cx("TechSection")}>
-                    <header className={cx("SectionHeader")}>
-                        <div className={cx("SectionTitleWrapper")}>
-                            <p className={cx("SectionTitle")}>{"Tech :"}</p>
-                            <button className={cx("SectionButton")}>
-                                {"More"}
-                            </button>
-                        </div>
-                        <div className={cx("SectionSubtitleWrapper")}>
-                            technical insights
-                        </div>
-                    </header>
-                    <main className={cx("SectionContent")}></main>
-                </section>
+  return (
+    <div className={cx("Wrapper")}>
+      <div className={cx("Container")}>
+        <motion.section
+          key="tech"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.5 }}
+          className={cx("TechSection")}
+        >
+          <header className={cx("SectionHeader")}>
+            <div className={cx("SectionTitleWrapper")}>
+              <p className={cx("SectionTitle")}>{"Tech :"}</p>
+              <button className={cx("SectionButton")} onClick={handleMoreClick}>
+                {"More"}
+              </button>
             </div>
-        </div>
-    );
+            <div className={cx("SectionSubtitleWrapper")}>
+              technical insights
+            </div>
+          </header>
+          <main className={cx("SectionContent")}></main>
+        </motion.section>
+        <section className={cx("TechSection")}>
+          <header className={cx("SectionHeader")}>
+            <div className={cx("SectionTitleWrapper")}>
+              <p className={cx("SectionTitle")}>{"Tech :"}</p>
+              <button className={cx("SectionButton")}>{"More"}</button>
+            </div>
+            <div className={cx("SectionSubtitleWrapper")}>
+              technical insights
+            </div>
+          </header>
+          <main className={cx("SectionContent")}></main>
+        </section>
+        <section className={cx("TechSection")}>
+          <header className={cx("SectionHeader")}>
+            <div className={cx("SectionTitleWrapper")}>
+              <p className={cx("SectionTitle")}>{"Tech :"}</p>
+              <button className={cx("SectionButton")}>{"More"}</button>
+            </div>
+            <div className={cx("SectionSubtitleWrapper")}>
+              technical insights
+            </div>
+          </header>
+          <main className={cx("SectionContent")}></main>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default HomeView;
